bc3f6c851e360cf14af532f7e35c60902f73aa81
Nikita Novik <friendoye@gmail.com>
2020-08-06 23:32:09 +0300

    Setup few Unit tests

---
+++ library/src/test/java/com/friendoye/recyclerxray/ColorGenerationTest.kt
@@ -0,0 +1,62 @@
+package com.friendoye.recyclerxray
+
+import android.graphics.Color
+import com.friendoye.recyclerxray.internal.generateColorSequence
+import com.friendoye.recyclerxray.internal.provideDefaultColorGeneratorRandom
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class ColorGenerationTest {
+
+    @Test
+    fun `5 generated colors have evenly distributed hue`() {
+        `test n generated colors have evenly distributed hue`(colorsAmount = 5)
+    }
+
+    @Test
+    fun `10 generated colors have evenly distributed hue`() {
+        `test n generated colors have evenly distributed hue`(colorsAmount = 10)
+    }
+
+    @Test
+    fun `20 generated colors have evenly distributed hue`() {
+        `test n generated colors have evenly distributed hue`(colorsAmount = 20)
+    }
+
+    @Test
+    fun `50 generated colors have evenly distributed hue`() {
+        `test n generated colors have evenly distributed hue`(colorsAmount = 50)
+    }
+
+    @Test
+    fun `100 generated colors have evenly distributed hue`() {
+        `test n generated colors have evenly distributed hue`(colorsAmount = 100)
+    }
+
+    private fun `test n generated colors have evenly distributed hue`(colorsAmount: Int) {
+        val averageHueDistance = 360.0f / colorsAmount
+
+        val random = provideDefaultColorGeneratorRandom()
+        val hsvArray = FloatArray(3)
+        val generatedHues = random.generateColorSequence().take(colorsAmount)
+            .map {
+                Color.colorToHSV(it, hsvArray)
+                hsvArray[0] // Extract HUE component
+            }
+            .sorted().toMutableList()
+        val closestHueDistance = generatedHues
+            .apply { add(360.0f + first()) } // Add first element to the end,
+                                             // so we can compute HUE distance
+                                             // between extremes.
+            .zipWithNext { hue1, hue2 -> hue2 - hue1 }
+            .min() ?: throw IllegalStateException("Couldn't find min hue distance!")
+
+        println("Avg hue distance: $averageHueDistance")
+        println("Min closest hue distance: $closestHueDistance")
+
+        Assert.assertTrue(averageHueDistance / 3 <= closestHueDistance)
+    }
+}
---
+++ library/src/test/java/com/friendoye/recyclerxray/stubs/StubViewHolders.kt
@@ -0,0 +1,28 @@
+package com.friendoye.recyclerxray.stubs
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+
+class DefaultViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+class ViewBindingViewHolder(
+    private val viewBinding: ViewBinding
+) : RecyclerView.ViewHolder(viewBinding.root)
+
+class PrimitiveArgsViewHolder(
+    private val integer: Int,
+    private val string: String,
+    private val boolean: Boolean,
+    private val nullableInteger: Int?,
+    private val nullableString: String?,
+    private val nullableBoolean: Boolean?,
+    private val nullableAny: Any?,
+    view: View
+) : RecyclerView.ViewHolder(view)
+
+class CustomArgsViewHolder(
+    private val first: FirstCustomClass,
+    private val second: SecondCustomClass,
+    view: View
+) : RecyclerView.ViewHolder(view)
