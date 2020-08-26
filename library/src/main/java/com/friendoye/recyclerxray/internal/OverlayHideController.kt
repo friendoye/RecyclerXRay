77a95cf416c5974731e4dcd29e27f84a80bb4373
Nikita Novik <friendoye@gmail.com>
2020-08-26 15:50:12 +0300

    Log OverlayHideController actions as internal logs do

--- library/src/main/java/com/friendoye/recyclerxray/internal/OverlayHideController.kt
+++ library/src/main/java/com/friendoye/recyclerxray/internal/OverlayHideController.kt
@@ -17,65 +17,69 @@ internal class OverlayHideController(
 
     fun toggleHidden(position: Int) {
         _isOverlayHidden[position] = !_isOverlayHidden[position]
-        Log.i("DataChange", _isOverlayHidden.joinToString())
+        Log.d(DEFAULT_INTERNAL_LOG_TAG, _isOverlayHidden.joinToString())
     }
 
     fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
         dataObserver = object : RecyclerView.AdapterDataObserver() {
             override fun onChanged() {
-                Log.i("DataChange", "onChanged()")
+                Log.d(DEFAULT_INTERNAL_LOG_TAG, "OverlayHideController: onChanged()")
                 recyclerView.handler.postAtFrontOfQueue {
                     _isOverlayHidden = (0..ownerAdapter.itemCount).map { false }.toMutableList()
-                    Log.i("DataChange", _isOverlayHidden.joinToString())
+                    logOverlayHiddenFlagsState()
                 }
             }
 
             override fun onItemRangeChanged(positionStart: Int, itemCount: Int) {
-                Log.i("DataChange", "onItemRangeChanged($positionStart, $itemCount)")
-                // Do nothing, because it is not structural change
+                Log.d(DEFAULT_INTERNAL_LOG_TAG, "OverlayHideController: onItemRangeChanged($positionStart, $itemCount)")
                 if (!isInXRayModeProvider()) {
                     (0..itemCount).map { index ->
                         _isOverlayHidden[positionStart + index] = false
                     }
+                    logOverlayHiddenFlagsState()
                 }
+                
             }
 
             override fun onItemRangeChanged(positionStart: Int, itemCount: Int, payload: Any?) {
-                Log.i("DataChange", "onItemRangeChanged($positionStart, $itemCount, $payload)")
-                // Do nothing, because it is not structural change
+                Log.d(DEFAULT_INTERNAL_LOG_TAG, "OverlayHideController: onItemRangeChanged($positionStart, $itemCount, $payload)")
                 if (!isInXRayModeProvider()) {
                     (0..itemCount).map { index ->
                         _isOverlayHidden[positionStart + index] = false
                     }
+                    logOverlayHiddenFlagsState()
                 }
             }
 
             override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
-                Log.i("DataChange", "onItemRangeInserted($positionStart, $itemCount)")
+                Log.d(DEFAULT_INTERNAL_LOG_TAG, "OverlayHideController: onItemRangeInserted($positionStart, $itemCount)")
                 repeat(itemCount) {
                     _isOverlayHidden.add(positionStart, false)
                 }
-                Log.i("DataChange", _isOverlayHidden.joinToString())
+                logOverlayHiddenFlagsState()
             }
 
             override fun onItemRangeRemoved(positionStart: Int, itemCount: Int) {
-                Log.i("DataChange", "onItemRangeRemoved($positionStart, $itemCount)")
+                Log.d(DEFAULT_INTERNAL_LOG_TAG, "OverlayHideController: onItemRangeRemoved($positionStart, $itemCount)")
                 repeat(itemCount) {
                     _isOverlayHidden.removeAt(positionStart)
                 }
-                Log.i("DataChange", _isOverlayHidden.joinToString())
+                logOverlayHiddenFlagsState()
             }
 
             override fun onItemRangeMoved(fromPosition: Int, toPosition: Int, itemCount: Int) {
-                Log.i("DataChange", "onItemRangeMoved($fromPosition, $toPosition, $itemCount)")
+                Log.d(DEFAULT_INTERNAL_LOG_TAG, "OverlayHideController: onItemRangeMoved($fromPosition, $toPosition, $itemCount)")
                 val areHidden = (0..itemCount).map { _isOverlayHidden[fromPosition] }
                 _isOverlayHidden.addAll(toPosition, areHidden)
                 repeat (itemCount) { _isOverlayHidden.removeAt(fromPosition) }
-                Log.i("DataChange", _isOverlayHidden.joinToString())
+                logOverlayHiddenFlagsState()
             }
         }
+        
+        Log.d(DEFAULT_INTERNAL_LOG_TAG, "OverlayHideController: onAttachedToRecyclerView()")
         _isOverlayHidden = (0 until ownerAdapter.itemCount).map { false }.toMutableList()
-        Log.i("DataChange", _isOverlayHidden.joinToString())
+        logOverlayHiddenFlagsState()
+        
         dataObserver?.let {
             ownerAdapter.registerAdapterDataObserver(it)
         }
@@ -86,5 +90,9 @@ internal class OverlayHideController(
             ownerAdapter.unregisterAdapterDataObserver(it)
         }
         dataObserver = null
+    }
+    
+    private fun logOverlayHiddenFlagsState() {
+        Log.d(DEFAULT_INTERNAL_LOG_TAG, _isOverlayHidden.joinToString())
     }
 }
