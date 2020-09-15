d28239e28abd1df5b8b90bade1a78f1edee3eabb
Nikita Novik <friendoye@gmail.com>
2020-09-15 17:38:25 +0300

    Apply linter

--- sample/src/androidTest/java/com/friendoye/recyclerxray/sample/SimpleRunCheckTest.kt
+++ sample/src/androidTest/java/com/friendoye/recyclerxray/sample/SimpleRunCheckTest.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.Lifecycle
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.rule.ActivityTestRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
